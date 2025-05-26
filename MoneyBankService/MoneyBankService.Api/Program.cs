76f30ec0525a36fb9c436a802e36e554f809d397
ZCardonaZ <santiagocardonan0625@gmail.com>
2025-05-25 19:06:01 -0500

    2222

---
+++ MoneyBankService/MoneyBankService.Api/Program.cs
@@ -0,0 +1,67 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoneyBankService.Api.Extensions;
+using MoneyBankService.Api.Middleware;
+using System.Text.Json; 
+using MoneyBankService.Infrastructure.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add the DB Context
+var connectionString = builder.Configuration.GetConnectionString("CnnStr"); // Obt�n la cadena de conexi�n
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)) // Sintaxis correcta para Pomelo
+);
+
+// Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options => 
+    {
+        
+       // options.JsonSerializerOptions.PropertyNamingPolicy = null;
+    })
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errorDetails = context.ConstructErrorMessages();
+            return new BadRequestObjectResult(errorDetails);
+        };
+    });
+
+// Add Fluent Validation
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add Modules
+builder.Services.AddRepositories();
+builder.Services.AddServices();
+builder.Services.AddMapping();
+builder.Services.AddValidators();
+
+
+builder.WebHost.UseUrls("http://*:80");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Add the Exception Middleware Handler
+app.UseExceptionMiddleware();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
